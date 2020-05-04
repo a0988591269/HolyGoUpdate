53a7f3ec2a1787d3b95832bb3285d46c31bb19bc
a0988591269 <53168882+a0988591269@users.noreply.github.com>
2020-05-04 15:57:54 +0800

    加入專案檔案。

---
+++ HolyGo/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using HolyGo.Repository;
+using HolyGo.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace HolyGo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public readonly HomeRepository _hr;
+
+        public HomeController()
+        {
+            _hr = new HomeRepository();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            //HomeRepository Home = new HomeRepository();
+            //var Travel = Home.topTravel().ToString();
+            //var Ticket = Home.topTicket().ToString();
+            //return View(Travel, Ticket);
+            var getTravelData = _hr.topTravel();
+            var getTicketData = _hr.topTicket();
+            //ViewData["key"] = model物件
+            //ViewBag.model = model物件
+            ViewData["topTravelData"] = getTravelData;
+            ViewData["topTicketData"] = getTicketData;
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
